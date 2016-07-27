7baff6e37aa0fdbce5808d1d6e22eaed458fef95
Rishabh <rishabh.gupta998@gmail.com>
2016-07-27 23:24:21 +0530

    Update About Examples Main Add Bootstrap to Package json

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,11 @@ var React=require('react');
 var About=React.createClass({
     render:function(){
       return(
-           <h3>About Component</h3>
+          <div >
+             <h1 className="text-center">About Page</h1>
+             <p>Welcome to the About Page!!!</p>
+          </div>
+
       )
 
     }
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,20 @@
 var React=require('react');
+var {Link}=require('react-router');
 
 var Example=React.createClass({
       render:function(){
           return(
             <div>
-                <h3>Example Component!!!!</h3>
+                <h1 className="text-center">Examples</h1>
+                <p>Here are few examples</p>
+                <ol>
+                   <li>
+                       <Link to='/?location=Roorkee'>Roorkee</Link>
+                   </li>
+                   <li>
+                       <Link to='/?location=Rishikesh'>Rishikesh</Link>
+                   </li>
+                </ol>
             </div>
           )
       }
